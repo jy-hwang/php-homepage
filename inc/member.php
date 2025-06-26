5883c7ff57c24f517b23cae190fffb281410cbe2
jyhwang <stratojy@gmail.com>
2025-06-27 00:19:51 +0900

    feat. id 중복 체크용 메서드 생성

---
+++ inc/member.php
@@ -0,0 +1,22 @@
+
+<?php
+// Member  Class file
+
+class Member {
+  private $conn;
+
+  public function __construct($db) { 
+    $this->conn = $db;
+
+  }
+
+  // id 중복 체크용 멤버 함수, 메서드
+   public function id_exist($m_id){
+    $sql = " SELECT * FROM member WHERE id=:m_id ";
+    $stmt = $this -> conn -> prepare($sql);
+    $stmt -> bindParam(':m_id',$m_id);
+    $stmt -> execute();
+
+    return $stmt -> rowCount() ?  true : false;
+   }
+}
--- temp_connection.php
+++ temp_connection.php
@@ -1,3 +1,13 @@
 <?php
 
 include 'inc/db_config.php';
+include 'inc/member.php';
+
+$id = 'user11';
+
+$mem = new Member($db);
+if($mem->id_exist($id)){
+  echo "아이디가 중복됩니다.";
+} else {
+  echo "사용할 수 있는 아이디 입니다.";
+}
