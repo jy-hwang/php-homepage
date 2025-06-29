809a8aa14ac1ef5889b239a4118e7d6ccb94fb68
jyhwang <stratojy@gmail.com>
2025-06-29 14:46:19 +0900

    feat. 회원가입 - 비밀번호 일치여부 확인
    1. 각 버튼에 type 을 주지 않으면 기본값이 'submit'으로 동작함.
    2. 비밀번호 일치여부는 가입확인을 눌렀을 때 확인할 수 있게 작업
    3. 가입확인을 눌렀을 때, 아이디 입력 부터 중복검사, 비밀번호 입력, 일치확인  등의 유효성검사를 진행

--- js/member_input.js
+++ js/member_input.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // id 중복검사
   const btn_id_check = document.querySelector("#btn_id_check");
 
   btn_id_check.addEventListener("click", () => {
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     xhr.onload = () => {
       if (xhr.status == 200) {
         const data = JSON.parse(xhr.responseText);
+        console.log(data);
         if (data.result == "success") {
           alert("사용이 가능한 아이디 입니다.");
           document.input_form.id_chk.value = "1";
@@ -31,5 +33,45 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       }
     };
+  });
+
+  // 가입확인 버튼 클릭시
+  const btn_submit = document.querySelector("#btn_submit");
+  btn_submit.addEventListener("click", () => {
+    // 아이디 입력 확인
+    const frm = document.input_form;
+    if (frm.f_id.value == "") {
+      alert("아이디를 입력해주세요");
+      frm.f_id.focus();
+      return false;
+    }
+
+    // 아이디 중복 검사 여부 확인
+    if (frm.id_chk.value === "0") {
+      alert("아이디 중복확인을 해주시기 바랍니다.");
+      return false;
+    }
+
+    // 비밀번호 입력 확인
+    if (frm.f_password.value == "") {
+      alert("비밀번호를 입력해주세요");
+      frm.f_password.focus();
+      return false;
+    }
+
+    if (frm.f_password2.value == "") {
+      alert("확인용 비밀번호를 입력해주세요");
+      frm.f_password2.focus();
+      return false;
+    }
+
+    // 비밀번호 일치 확인
+    if (frm.f_password.value != frm.f_password2.value) {
+      alert("비밀번호가 서로 일치하지 않습니다.\n다시 입력해주세요");
+      frm.f_password.value = "";
+      frm.f_password2.value = "";
+      frm.f_password.focus();
+      return false;
+    }
   });
 });
--- member_input.php
+++ member_input.php
@@ -18,20 +18,20 @@ include 'inc_header.php';
     <div class="form-group d-flex gap-2 align-items-end">
       <div>
         <label for="f_id" class="form-label">아이디</label>
-        <input type="text" class="form-control" name="" id="f_id" placeholder="아이디를 입력해주세요">
+        <input type="text" class="form-control" name="f_id" id="f_id" placeholder="아이디를 입력해주세요">
       </div>
-      <button class="btn btn-secondary" id="btn_id_check">아이디 중복확인</button>
+      <button type="button" class="btn btn-secondary" id="btn_id_check">아이디 중복확인</button>
     </div>
 
     <div class="form-group mt-3 d-flex gap-2 justify-content-between">
       <div class="w-50">
         <label for="f_password" class="form-label">비밀번호</label>
-        <input type="password" class="form-control" name="" id="f_password" placeholder="비밀번호를 입력해주세요">
+        <input type="password" class="form-control" name="f_password" id="f_password" placeholder="비밀번호를 입력해주세요">
       </div>
 
       <div class="w-50">
         <label for="f_password2" class="form-label">비밀번호 확인</label>
-        <input type="password" class="form-control" name="" id="f_password2" placeholder="비밀번호를 입력해주세요">
+        <input type="password" class="form-control" name="f_password2" id="f_password2" placeholder="비밀번호를 입력해주세요">
       </div>
     </div>
 
@@ -40,7 +40,7 @@ include 'inc_header.php';
         <label for="f_email" class="form-label">이메일</label>
         <input type="email" class="form-control" name="" id="f_email" placeholder="이메일을 입력해주세요">
       </div>
-      <button class="btn btn-secondary">이메일 중복확인</button>
+      <button type="button" class="btn btn-secondary">이메일 중복확인</button>
     </div>
 
     <div class="d-flex mt-3 gap-2 align-items-end">
@@ -48,7 +48,7 @@ include 'inc_header.php';
         <label for="f_zipcode" class="form-label" >우편번호</label>
         <input type="text" class="form-control" name="zipcode" id="zipcode" minlength="5" maxlength="5">
       </div>
-      <button class="btn btn-secondary">우편번호찾기</button>
+      <button type="button" class="btn btn-secondary">우편번호찾기</button>
     </div>
 
     <div class="form-group mt-3 d-flex gap-2 justify-content-between">
@@ -72,8 +72,8 @@ include 'inc_header.php';
     </div>
 
     <div class="mt-3 d-flex gap-2">
-      <button class="btn btn-primary flex-grow-1">가입확인</button>
-      <button class="btn btn-secondary flex-grow-1">가입취소</button>
+      <button type="button" class="btn btn-primary flex-grow-1" id="btn_submit">가입확인</button>
+      <button type="button" class="btn btn-secondary flex-grow-1">가입취소</button>
     </div>
   </form>
 </main>
